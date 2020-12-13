06e9a6bb882425757f8df6d746f0165ebfd8fb60
ArtART241 <graubergerartem@gmail.com>
2020-12-13 13:01:15 +0100

    lesson 13.12

---
+++ script.js
@@ -0,0 +1,20 @@
+let music;
+
+$(document).ready(function () {
+    
+    music = new Howl({
+        src: ["audio/track2.mp3"],
+        loop: true,
+        volume: 0.75
+    });
+
+
+});
+
+$("#player>img").click(function (e) { 
+    if(e.target.id != "isPlaying") {
+        music.play();
+        e.target.id = "isPlaying";
+    }
+        
+});
