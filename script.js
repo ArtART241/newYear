c01db25b072c0f3002633d214dad9d4fff911ef7
ArtART241 <graubergerartem@gmail.com>
2021-01-10 10:04:43 +0100

    lole

--- script.js
+++ script.js
@@ -1,57 +1,153 @@
-let music;
-
-$(document).ready(function () {
-    
-    music = new Howl({
-        src: ["audio/track2.mp3", "audio/track1.mp3", "audio/track3.mp3"],
-        loop: true,
-        volume: 0.3
-    });
-
-
+// Список музыки
+let music1 = new Howl({
+    src: ["audio/track1.mp3"],
+    loop: true,
+    volume: 0.5
+});
+let music2 = new Howl({
+    src: ["audio/track2.mp3"],
+    loop: true,
+    volume: 0.5
+});
+let music3 = new Howl({
+    src: ["audio/track3.mp3"],
+    loop: true,
+    volume: 0.5
 });
 
-$("#tree>img").click(function (e) { 
-    $("#tree>img").removeClass("shaking");
-    music.stop();
-    let name = e.target.id
-    console.log(name);
-    if (name == "toy1") {
+// Проигрыш музыки при нажатии на игрушку (Компутер)
+$("#tree>img").click(function(e){ 
+    $("#tree>img").removeClass("shaking")
+    let name = e.target.id;
+
+    if(name == "toy1"){
         $(e.target).addClass("shaking");
-        music.play(1);
-    } else if (name == "toy2") {
-        $(e.target).addClass("shaking");
-        music.play(2);
-    } else if (name == "toy3") {
-        $(e.target).addClass("shaking");
-        music.play(3);
-    }
-    
+        music2.stop();
+        music3.stop();
         
+        music1.pause();
+        music1.play();
+    }
+    else if(name == "toy2"){
+        $(e.target).addClass("shaking");
+        music1.stop();
+        music3.stop();
+
+        music2.pause();
+        music2.play();
+    }
+    else if(name == "toy3"){
+        $(e.target).addClass("shaking");
+        music2.stop();
+        music1.stop();
+
+        music3.pause();
+        music3.play();
+    }
+});
+// Остановка музыки при нажатии на звезду (Компутер)
+$("#tree>#treeStar").click(function (e) { 
+    $("#tree>img").removeClass("shaking")
+    music1.stop();
+    music2.stop();
+    music3.stop();
+});
+
+// Проигрыш музыки при нажатии на игрушку (Смартфон)
+$("#treeMobile>img").click(function(e){ 
+    $("#treeMobile>img").removeClass("shaking")
+    let name = e.target.id;
+
+    if(name == "toy1"){
+        $(e.target).addClass("shaking");
+        music2.stop();
+        music3.stop();
+
+        music1.play();
+    }
+    else if(name == "toy2"){
+        $(e.target).addClass("shaking");
+        music1.stop();
+        music3.stop();
+
+        music2.play();
+    }
+    else if(name == "toy3"){
+        $(e.target).addClass("shaking");
+        music2.stop();
+        music1.stop();
+
+        music3.play();
+    }
+});
+// Остановка музыки при нажатии на звезду (Смартфон)
+$("#treeMobile>#treeStar").click(function (e) { 
+    $("#treeMobile>img").removeClass("shaking")
+    music1.stop();
+    music2.stop();
+    music3.stop();
 });
 
 
-
-
-let timer = setInterval(function(){
-    // Time of new year
+// Отсчёт до нового года
+setInterval(function(){
+    // Время нового года
     let newYear = new Date("1 January 2021 00:00:00");
-    // current time
-    let currentTime = new Date();
-    // difference
-    let diff = newYear - currentTime;
-    
-    let seconds = Math.floor(diff / 1000);
-    let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(minutes / 60);
-    let days = Math.floor(hours / 24);
+    // Нынешнее время
+    let now = new Date();
+    // Разность дат
+    let diff = newYear - now;
 
-    let days2 = hours % 24;
-    let hours2 = minutes % 60;
-    let minutes2 = seconds % 60;
+    // Проверяем что таймер не истёк
+    if(diff > 0){
+        let days = Math.floor( diff/(24*60*60*1000) );
+        let hours = Math.floor( (diff%(24*60*60*1000)) / (60*60*1000) );
+        let minutes = Math.floor( (diff%(60*60*1000)) / (60*1000));
+        let seconds = Math.floor( (diff%(60*1000)) / 1000);
 
-    $("#seconds").text(minutes2);
-    $("#minutes").text(hours2 + " :");
-    $("#hours").text(days2 + " :");
-    $("#days").text(days + " :");
-}, 1000);
+        $(".seconds").text(seconds);
+        $(".minutes").text(minutes + " :");
+        $(".hours").text(hours + " :");
+        $(".days").text(days + " :");
+    }
+    else{
+        $("#countdown p, #countdownMobile p").text("Новый Год Наступил!")
+        $("#countdown, #countdownMobile").fadeOut(2000, function(){
+            $(this).remove();
+            $(".envlope-wrapper").fadeIn(1000).removeClass("d-none")
+        });
+    }
+}, 1000)
+
+
+// Анимация шляпы снеговика
+$(".hat").click(function (e) { 
+    // Запускаем анимацию
+    $(e.currentTarget).addClass("hatBounce");
+
+    // Когда закончится анимация (1,5секунды) отключаем анимацию
+    setTimeout(function(){
+        $(e.currentTarget).removeClass("hatBounce");
+    }, 1500)
+});
+
+// Анимация махания рукой запускается каждые 3,5 секунды
+setInterval(function(){
+    // Запускаем анимацию
+    $(".hand-r").addClass("hand-rSwing");
+
+    // Когда закончится анимация (1,5секунды) отключаем анимацию
+    setTimeout(function(){
+        $(".hand-r").removeClass("hand-rSwing");
+    }, 1500)
+}, 3500)
+
+
+
+
+
+// Открываем и закрываем открытку
+$(".envlope-wrapper").click(function (e) { 
+    $("#envelope").toggleClass("open");
+    $("#envelope").toggleClass("close");
+});
